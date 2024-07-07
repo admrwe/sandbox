c563b11011f3aa51be99f6b8b97f20c2fd1f3ade
Adam Rowe <admrwe@Adams-MBP.gpcom.net>
2024-07-07 11:52:23 -0600

    chore: dialog cleanup

--- components/Dialog/Dialog.stories.tsx
+++ components/Dialog/Dialog.stories.tsx
@@ -5,6 +5,8 @@ import { Button } from '../Button';
 import { Stack } from '../Stack';
 import { Heading } from '../Heading';
 import { Text } from '../Text';
+import { Disclosure } from '../Disclosure';
+import { Input } from '../Input';
 
 import Dialog, { DialogProps } from './Dialog';
 
@@ -33,6 +35,34 @@ export const Default: Story = {
             <Dialog.Body title={args.title}>
               <Stack>
                 <Text>Hello world foo bar baz.</Text>
+                <Disclosure title="Disclosure title" defaultExpanded={true}>
+                  <Stack space="2-x">
+                    <Heading size="1-and-eigth-x" fontWeight="bold">
+                      Section title
+                    </Heading>
+                    <Text>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nam aliquet libero dolor, non scelerisque risus dictum
+                      quis. Nullam a mauris vitae purus convallis ultricies.
+                    </Text>
+                    <Input label="Hello world" />
+                    <Heading
+                      size="1-and-eigth-x"
+                      fontWeight="bold"
+                      marginBlockStart="2-x"
+                    >
+                      Section title
+                    </Heading>
+                    <Text>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nam aliquet libero dolor, non scelerisque risus dictum
+                      quis. Nullam a mauris vitae purus convallis ultricies.
+                    </Text>
+                    <Button style={{ alignSelf: 'flex-start' }}>
+                      Hello world
+                    </Button>
+                  </Stack>
+                </Disclosure>
                 <Button style={{ alignSelf: 'start' }} onClick={closeDialog}>
                   Child close
                 </Button>
